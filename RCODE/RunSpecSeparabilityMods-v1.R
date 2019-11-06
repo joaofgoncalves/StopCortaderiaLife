b88e46880d4be17bd444f71b7746de76ecaddd1c
JoaoGoncalves <joaofgo@gmail.com>
2019-11-06 16:29:22 +0000

    Multi-algorithm separability analyses

--- RCODE/RunSpecSeparabilityMods-v1.R
+++ RCODE/RunSpecSeparabilityMods-v1.R
@@ -6,108 +6,167 @@ library(caret)
 library(parallel)
 library(doParallel)
 
-cl <- makePSOCKcluster(5)
-registerDoParallel(cl)
+# Start a parallel cluster for boosting speed in caret processing
+#cl <- makePSOCKcluster(10)
+#registerDoParallel(cl)
 
-fl <- list.files("C:/MyFiles/R-dev/StopCortaderiaLife/DATA/TABLES/ReflDataBySensor", pattern=".csv$",
-                 full.names = TRUE)
 
+## --------------------------------------------------------------------------- ##
+
+# List the data
+fl <- list.files("C:/MyFiles/R-dev/StopCortaderiaLife/DATA/TABLES/ReflDataBySensor", 
+                 pattern=".csv$", full.names = TRUE)
+# Files by spec lib
 fl_CIBIO <- fl[1:13]
 fl_IHCant <- fl[14:26]
 
+# Read master files with calibration data i.e., Cortaderia vs. all the rest
 tb_CIBIO <- read.csv("./DATA/TABLES/Cortaderia_ReflProfilesField_Master-v2.csv")
 tb_IHCant <- read.csv("./DATA/TABLES/Cortaderia_ReflProfilesIHCantb_Master-v2.csv")
 
+# Initialize the list object that will hold all the caret train objects
 trainObjs <- list()
 
-for(i in 1:13){
-  
-  # Read average data from speclibs
-  DF1 <- read.csv(fl_CIBIO[i])
-  DF2 <- read.csv(fl_IHCant[i])
-  NC1 = ncol(DF1)
-  NC2 = ncol(DF2)
-  vnames = colnames(DF1)
-  
-  # Check if the number of columns is the same
-  if(NC1 != NC2){
-    cat("Found differences for the following tables:\n->",
-        basename(fl_CIBIO[i]),"\n->",basename(fl_IHCant[i]),"\n\n")
-    #next
-    
-    vnames <- base::intersect(colnames(DF1), colnames(DF2)) 
-    DF1 <- DF1[,vnames]
-    DF2 <- DF2[,vnames]
-  }
-  
-  # Assemble data
-  trainDF <-
-  rbind(data.frame(Class = tb_CIBIO$Binary, DF1),
-        data.frame(Class = tb_IHCant$Binary, DF2)
-  )
-  trainDF[,"Class"] <- as.factor(trainDF[,"Class"])
-  
-  ctrlObj <- trainControl(method = "LGOCV", 
-                          number = 50, 
-                          p = 0.50, 
-                          verboseIter = TRUE)
+# List of ML-classification methods to test and 'tunable' parameters
+#
+# rf          - Random forest (mtry)
+# C5.0Tree    - Single C5.0 Tree (none)
+# adaboost    - AdaBoost Classification Trees (nIter, method)
+# Linda       - Robust Linear Discriminant Analysis (none)
+# gam         - Generalized Additive Models (select, method)
+# kknn        - k-nearest neighbors (kmax, distance, kernel)
+# lssvmRadial - Least Squares Support Vector Machine with Radial Basis Function Kernel (sigma, tau)
+# svmRadial   - Support Vector Machines with Radial Basis Function Kernel (sigma, C)
+# nnet        - Neural Network (size, decay)
+#
+mlMethods <- c("rf", "C5.0Tree", "adaboost", "Linda", "gam", 
+               "kknn","lssvmRadial", "svmRadial", "nnet")
 
-  trainObj <- train(trainDF[,-1], 
-                    y = trainDF[,1], 
-                    # method = "rf",       # Random Forest
-                    # method = "C5.0Tree", # Single C5.0 Tree
-                    # method = "Linda",    # Robust Linear Discriminant Analysis
-                    # method = "qda",      # Quadratic Discriminant Analysis
-                    # method = "lda",
-                    # method = "kknn",
-                    # method = "gam",         # Generalized Additive Model using Splines !!SLOW!!
-                    # NOT WORKING: method="fda",
-                    # method = "xgbLinear",   # eXtreme Gradient Boosting !!SLOW!!
-                    # method = "adaboost",    # AdaBoost Classification Trees
-                    # NOT WORKING: method = "bagFDA",
-                    # NOT WORKING: method = "bagEarth",
-                    # NOT WORKING method = "bayesglm", # Bayesian Generalized Linear Model
-                    # NOT WORKING: method = "binda",
-                    # NOT WORKING: method = "blackboost",
-                    # NOT WORKING: method = "bstTree",
-                    # NOT WORKING: method = "randomGLM",
-                    # NOT WORKING: method = "SLAVE",
-                    # method = "gaussprLinear", # !!!VERY SLOW!!!
-                    # NOT WORKING: method = "glm",
-                    # NOT WORKING: method = "glmnet",
-                    # NOT WORKING: method = "lvq",
-                    # method = "lssvmRadial",
-                    # method = "avNNet", # Model Averaged Neural Network !! Unstable??
-                    # NOT WORKING: method = "gcvEarth",
-                    method = "snn",
-                    trControl = ctrlObj, 
-                    allowParallel = TRUE)
+
+## --------------------------------------------------------------------------- ##
+
+# Define the control object for caret using Holdout or Group out 
+# cross-validation 50/50 % train/test
+#
+ctrlObj <- trainControl(method = "LGOCV", 
+                        number = 500, 
+                        p = 0.50, 
+                        verboseIter = FALSE)
+
+k <- 0
+for(mlMethod in mlMethods){ # Iterate through all the ML-methods
   
-  tmpPerfData <- data.frame(
-    Sensor = gsub("CIBIOdata_|.csv","",basename(fl_CIBIO[i])),
-    NC1 = NC1,
-    NC2 = NC2,
-    mxKappa = max(trainObj$results$Kappa),
-    mxAccuracy = max(trainObj$results$Accuracy),
-    varNames = paste(vnames,collapse = " | "),
-             stringsAsFactors = FALSE)
   
-  if(i==1){
-    perfScores <- tmpPerfData
-  }else{
-    perfScores <- rbind(perfScores, tmpPerfData)
+  for(i in 1:13){ # Iterate through all sensors/platforms
+    
+    # Read the averaged data from each speclib
+    DF1 <- read.csv(fl_CIBIO[i])
+    DF2 <- read.csv(fl_IHCant[i])
+    NC1 = ncol(DF1)
+    NC2 = ncol(DF2)
+    vnames = colnames(DF1)
+    
+    # Check if the number of columns is the same in each 
+    # Spectral Library (CIBIO and IH Cantabria)
+    # IF NOT, then subset to the intersection of bands 
+    # available from both spectroradiometers
+    #
+    if(NC1 != NC2){
+      cat("Found differences for the following tables:\n->",
+          basename(fl_CIBIO[i]),"\n->",basename(fl_IHCant[i]),"\n\n")
+      
+      # Calculate the intersection between the bands available in each dataset
+      vnames <- intersect(colnames(DF1), colnames(DF2)) 
+      DF1 <- DF1[,vnames]
+      DF2 <- DF2[,vnames]
+    }
+    
+    # Assemble the data from both spec libs
+    trainDF <-
+      rbind(data.frame(Class = tb_CIBIO$Binary, DF1),
+            data.frame(Class = tb_IHCant$Binary, DF2)
+      )
+    
+    # Convert the train column to factor object (named 'Class')
+    trainDF[,"Class"] <- as.factor(trainDF[,"Class"])
+    
+    # Train the classifier using
+    trainObj <- train(x = trainDF[,-1], 
+                      y = trainDF[,1], 
+                      # method = "rf",       # Random Forest
+                      # method = "C5.0Tree", # Single C5.0 Tree
+                      # method = "Linda",    # Robust Linear Discriminant Analysis
+                      # method = "qda",      # Quadratic Discriminant Analysis
+                      # method = "lda",
+                      # method = "kknn",
+                      # method = "gam",         # Generalized Additive Model using Splines !!SLOW!!
+                      # NOT WORKING: method="fda",
+                      # method = "xgbLinear",   # eXtreme Gradient Boosting !!SLOW!!
+                      # method = "adaboost",    # AdaBoost Classification Trees
+                      # NOT WORKING: method = "bagFDA",
+                      # NOT WORKING: method = "bagEarth",
+                      # NOT WORKING method = "bayesglm", # Bayesian Generalized Linear Model
+                      # NOT WORKING: method = "binda",
+                      # NOT WORKING: method = "blackboost",
+                      # NOT WORKING: method = "bstTree",
+                      # NOT WORKING: method = "randomGLM",
+                      # NOT WORKING: method = "SLAVE",
+                      # method = "gaussprLinear", # !!!VERY SLOW!!!
+                      # NOT WORKING: method = "glm",
+                      # NOT WORKING: method = "glmnet",
+                      # NOT WORKING: method = "lvq",
+                      # method = "lssvmRadial",
+                      # method = "avNNet", # Model Averaged Neural Network !! Unstable??
+                      # NOT WORKING: method = "gcvEarth",
+                      #method = "snn",
+                      method = mlMethod,
+                      trControl = ctrlObj, 
+                      allowParallel = TRUE)
+    
+    # Make a temporary data frame to store performance values from caret train object
+    tmpPerfData <- data.frame(
+      Sensor = gsub("CIBIOdata_|.csv","",basename(fl_CIBIO[i])), # Sensor name
+      NC1 = NC1,                                                 # Number of columns/bands in CIBIO data
+      NC2 = NC2,                                                 # Number of columns in IHC data
+      mxKappa = max(trainObj$results$Kappa),                     # Best Kappa
+      mxAccuracy = max(trainObj$results$Accuracy),               # Best Accuracy
+      varNames = paste(vnames,collapse = " | "),                 # Band names used (intersection between both datasets)
+      stringsAsFactors = FALSE)                  
+    
+    # Append performance scores across different sensors 
+    if(i==1){
+      perfScores <- tmpPerfData
+    }else{
+      perfScores <- rbind(perfScores, tmpPerfData)
+    }
+    
+    # Store train objects from caret
+    trainObjs[[i]] <- trainObj
+    
+    # Print the main results in caret train object
+    cat("\n\n-------------------------------------------------------------------------\n")
+    cat("->",basename(fl_CIBIO[i]),"\n->",basename(fl_IHCant[i]),"\n\n")
+    cat("->",toupper(mlMethod),"\n")
+    print(trainObj)
+    cat("-------------------------------------------------------------------------\n\n")
+    
   }
   
-  trainObjs[[i]] <- trainObj
+  # Append results by ML-classification method
+  # Make temp data frame 
+  TMP <- data.frame(mlMethod=mlMethod, perfScores)
   
-  cat("\n\n-------------------------------------------------------------------------\n")
-  cat("->",basename(fl_CIBIO[i]),"\n->",basename(fl_IHCant[i]),"\n\n")
-  print(trainObj)
-  cat("-------------------------------------------------------------------------\n\n")
+  k <- k+1
+  if(k==1){
+    perfScoresByMLmethod <- TMP
+  }else{
+    perfScoresByMLmethod <- rbind(perfScoresByMLmethod, TMP)
+  }
   
 }
 
-arrange(perfScores,desc(mxKappa)) %>% 
-  mutate(mxKappa=round(mxKappa,2), mxAccuracy=round(mxAccuracy,2))
+# Arrange/sort the output scores
+arrange(perfScoresByMLmethod,mlMethod,desc(mxKappa)) %>% 
+  mutate(mxKappa=round(mxKappa,3), mxAccuracy=round(mxAccuracy,3))
 
 
