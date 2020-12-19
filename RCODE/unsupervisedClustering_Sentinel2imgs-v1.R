e41db68e1090f8e48c5b3f4f746c6fa41cb56a6c
JoaoGoncalves <joaofgo@gmail.com>
2020-12-19 18:16:13 +0000

    Unsupervised k-means S2 images (too slow)

--- RCODE/unsupervisedClustering_Sentinel2imgs-v1.R
+++ RCODE/unsupervisedClustering_Sentinel2imgs-v1.R
@@ -7,17 +7,34 @@ library(dplyr)
 library(tidyr)
 library(RStoolbox)
 library(raster)
+library(crayon)
+#source("./RCODE/_INPUT_PARAMS.R")
 
-source("./RCODE/_INPUT_PARAMS.R")
+
+dirsToProc <- list.dirs("D:/DATA/LifeCortaderia/S2_IMAGES/SENTINEL_2019_ALL",
+                        recursive = FALSE)
+
+N <- length(dirsToProc)
+pb <- txtProgressBar(min = 1, max = N, style = 3)
 
 
-
-r <- stack(sceneList[[3]])
-names(r) <- sceneBandNames[[3]]
-
-unC <- unsuperClass(r, nSamples = 1E6, nClasses = 20, nStarts = 20)
-
-writeRaster(unC$map, "D:/DATA/LifeCortaderia/Km_20c_scn3.tif")
+for(i in 1:N){
+  
+  dirProc <- dirsToProc[i]
+  tileName <- basename(dirProc)
+  
+  fl <- list.files(dirProc, pattern=".tif$", full.names = TRUE)
+  
+  flStack <- fl[grepl("_201908",fl)]
+  rStack <- stack(flStack)
+  
+  unC <- unsuperClass(rStack, nSamples = 1E6, nClasses = 20, nStarts = 20)
+  fout <- paste("D:/DATA/LifeCortaderia/Kmeans_20c_S2L2A_201908_",tileName,"_v1.tif",sep="")
+  writeRaster(unC$map, fout)
+  
+  cat(green("\n\n-> Finished processing tile: [",tileName,"]\n\n"))
+  setTxtProgressBar(pb, i)
+}
 
 
 
