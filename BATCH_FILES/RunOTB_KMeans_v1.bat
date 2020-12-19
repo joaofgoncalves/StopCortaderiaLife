dfbd1c1b3e31414aadfddd104111dbed02354c4c
JoaoGoncalves <joaofgo@gmail.com>
2020-12-19 18:17:00 +0000

    Run OTB K-means batch file (much faster overall!)

---
+++ BATCH_FILES/RunOTB_KMeans_v1.bat
@@ -0,0 +1,2 @@
+
+otbcli_KMeansClassification.bat -in D:/DATA/LifeCortaderia/S2_IMAGES/SENTINEL_2019_ALL_CROP/T29TNG/S2B_MSIL2A_20190821T113329_N0213_R080_T29TNG_20190821T141742_crop_int32.tif -out D:/DATA/LifeCortaderia/S2_IMAGES/km_20c_T29TNG_201908_v1.tif uint8 -nc 20 -ts 100000 -maxit 200 -ram 4000 -sampler random -rand 12345
