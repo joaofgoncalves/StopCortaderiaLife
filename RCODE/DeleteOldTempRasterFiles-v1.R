6baad5de415644616cac484c9bdf171535959954
JoaoGoncalves <joaofgo@gmail.com>
2020-12-19 18:15:30 +0000

    Delete temp files from the raster tempdir (causing system crash)

---
+++ RCODE/DeleteOldTempRasterFiles-v1.R
@@ -0,0 +1,29 @@
+
+
+
+ageoffile <- function(x){
+  dt = difftime(Sys.time(), file.info(x)$mtime, units="mins")
+  return(dt)
+}
+
+for(i in 1:10000){
+  
+  fl <- list.files("C:/temp/RtmpWofEcg/raster", full.names = TRUE)
+  
+  for(fn in fl){
+    
+    fage <- ageoffile(fn)
+    print(fage)
+    
+    if(fage >= 12){
+      cat("Removing file:\n")
+      cat(paste(" ->",fn,"\n\n"))
+      file.remove(fn)
+    }
+  }
+  cat("Sleeping now....\n\n\n")
+  Sys.sleep(60)
+}
+
+
+
